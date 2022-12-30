3ba38d1947bf6425927159a56613babf5df3f686
Peter Walsh <62070072+p8walsh@users.noreply.github.com>
2022-12-29 19:37:03 -0700

    Update UniversityOfIowaColors.py

--- UniversityOfIowaColors.py
+++ UniversityOfIowaColors.py
@@ -3,18 +3,18 @@ import neopixel
 import time
 
 num_pixels = 150
-pixels = neopixel.NeoPixel(board.D18, num_pixels, brightness=0.2, auto_write=False)
+pixels = neopixel.NeoPixel(board.D18, num_pixels, brightness=0.8, auto_write=False)
 
 x = 0
 while True: 
     for i in range(0, num_pixels-10, 10):
         for j in range(10):
-            if ( i % 2 == x ):
-                pixels[i+j] = (255, 205, 0)
+            if ( j < 5 ):
+                pixels[(i+j+x)%num_pixels] = (255,205,0)
             else:
-                pixels[i+j] = (0, 0, 0)
+                pixels[(i+j+x)%num_pixels] = (40,40,40)
     
     pixels.show()
-    time.sleep(0.1)
+    time.sleep(1)
     
-    x = (x + 1) % 2
+    x = (x + 1) % num_pixels
