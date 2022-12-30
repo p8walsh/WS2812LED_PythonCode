5087860898290d1ad4cb3f16a279bdaa058d7346
Peter Walsh <62070072+p8walsh@users.noreply.github.com>
2022-12-29 18:48:00 -0700

    Update CornellCollegeColors.py

--- CornellCollegeColors.py
+++ CornellCollegeColors.py
@@ -3,13 +3,13 @@ import neopixel
 import time
 
 num_pixels = 150
-pixels = neopixel.NeoPixel(board.D18, num_pixels, brightness=0.2, auto_write=False)
+pixels = neopixel.NeoPixel(board.D18, num_pixels, brightness=0.8, auto_write=False)
 
 x = 0
 while True: 
     for i in range(0, num_pixels-10, 10):
         for j in range(10):
-            if ( i % 2 == x ):
+            if ( (i/10) % 2 == x ):
                 pixels[i+j] = (82, 49, 120)
             else:
                 pixels[i+j] = (255, 255, 255)
