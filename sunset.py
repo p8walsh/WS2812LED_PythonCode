dd81ccf4699b47d2a6ace5253e3d45eeef67ed64
Peter Walsh <peterbug80228@gmail.com>
2023-08-27 21:08:14 -0600

    Remove Print Message

--- sunset.py
+++ sunset.py
@@ -7,6 +7,6 @@ longitude = -104.879104
 sun = Sun(latitude, longitude)
 
 today_sunset = sun.get_local_sunset_time()
-print(type(today_sunset.hour))
+#print(type(today_sunset.hour))
 with open("sunset_time.txt", 'w') as file:
     file.write(f'{today_sunset.hour}'.zfill(2) + ":" + f'{today_sunset.minute}'.zfill(2))
